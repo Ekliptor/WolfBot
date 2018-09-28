f83d301f03443bcad6c0147235ea90f69a0b8297
Ekliptor <Ekliptor1@yandex.com>
2018-09-28 21:23:35 +0700

    DB reconnect improvement on cluster

--- src/Controller.ts
+++ src/Controller.ts
@@ -386,7 +386,7 @@ export class Controller extends AbstractController { // TODO implement graceful 
         if (!err || typeof err !== "object")
             return false;
         let message = (err.message || "") + " " + (err.name || ""); // err.stack is too long
-        if (message && message.indexOf('MongoError: ') !== -1 && (
+        if (message && message.indexOf('MongoError') !== -1 && (
             message.indexOf('timed out') !== -1 || message.indexOf('slaveOk=false') !== -1 || message.indexOf('Topology was destroyed') !== -1)) {
             // MongoError: connection 5 to localhost:27017 timed out
             this.log('Database connection error', err, err.stack)
