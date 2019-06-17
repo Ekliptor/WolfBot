5b8dc464d052e05cbc53e5689e4c86d7fedf2f89
Ekliptor <Ekliptor1@yandex.com>
2019-06-17 16:26:49 +0700

    added swap interest strategy idea

---
+++ src/Strategies/SwapInterestCollector.ts
@@ -0,0 +1,5 @@
+
+// TODO
+// 1. add interest API calls for OKEX and BitMEX and Deribit perpetual swaps
+// 2. open long/short position to collect interest
+// 3. optionally close positions when interest changes for x candles or increase size to close at profit
