343f07251a7336a5f4995f26ecfbcd9ed3635920
Ekliptor <daniel@blockfint.com>
2021-02-13 14:07:44 +0700

    added indicator suggestions

---
+++ src/Indicators/ADL.ts
@@ -0,0 +1,5 @@
+
+// TODO add Accumulation Distribution Line
+// a more advanced OBV taking high/low/close + volume into account
+// https://school.stockcharts.com/doku.php?id=technical_indicators:accumulation_distribution_line
+// not in talib
---
+++ src/Indicators/CVD.ts
@@ -0,0 +1,4 @@
+
+// TODO add Cumulative Volume Delta as alternative to OBV
+// https://coinalyze.net/blog/cumulative-volume-delta-cvd-indicator-analyzing-buyer-and-seller-activities/
+// not in talib
